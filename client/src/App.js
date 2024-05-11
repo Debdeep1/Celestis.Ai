eff84a3e57f2329debe5576503006d4411fca6c1
Debdeep1 <deb.baner.1@gmail.com>
2024-05-11 20:46:34 +0530

    Interview Style Initialized

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import PromptBox from './components/PromptBox';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,12 +20,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <PromptBox />
+    </div>
   )
 }
 
