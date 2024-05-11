1c733c025525db4660d0f893ae7bd045cf4ec72b
Debdeep1 <deb.baner.1@gmail.com>
2024-05-11 17:39:01 +0530

    firebase init

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBH9iTJMN_ogclhIQ8I0qEEYiOlN1Utzo4",
+  authDomain: "celestis-ai.firebaseapp.com",
+  projectId: "celestis-ai",
+  storageBucket: "celestis-ai.appspot.com",
+  messagingSenderId: "442132310768",
+  appId: "1:442132310768:web:e2d2041aa162ca7728f035",
+  measurementId: "G-P66EKS7XBB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
